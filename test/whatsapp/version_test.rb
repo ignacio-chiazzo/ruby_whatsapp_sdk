b89a5e92e4d6e6f419054284743b3f311f0d8442
ignacio-chiazzo <ignaciochiazzo@gmail.com>
2022-05-29 16:50:37 -0400

    foo

--- test/whatsapp/version_test.rb
+++ test/whatsapp/version_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
-
+require "whatsapp_sdk"
 class VersionTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::WhatsappSdk::VERSION
