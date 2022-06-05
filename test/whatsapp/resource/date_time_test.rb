7012e66fc463dbe31c1bb9c8313846e204c3b69b
ignacio-chiazzo <ignaciochiazzo@gmail.com>
2022-06-05 09:10:34 -0400

    Added Currency and DateTime resources

---
+++ test/whatsapp/resource/date_time_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require_relative '../../../lib/whatsapp_sdk/resource/date_time'
+
+module WhatsappSdk
+  module Resource
+    module Resource
+      class DateTimeTest < Minitest::Test
+        def test_to_json
+          date_time = WhatsappSdk::Resource::DateTime.new(fallback_value: "2020-01-01T00:00:00Z")
+          assert_equal({ fallback_value: "2020-01-01T00:00:00Z" }, date_time.to_json)
+        end
+      end
+    end
+  end
+end
