1a81a7d574581a12d0a4400d3adeb67ba982cea9
ignacio-chiazzo <ignaciochiazzo@gmail.com>
2022-06-05 09:11:00 -0400

    Added parameters and components

---
+++ lib/whatsapp_sdk/resource/parameter_object.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+# TODO: Do I need it?
+module WhatsappSdk
+  module Resource
+    class ParameterObject
+      class InvalidType < StandardError
+        attr_accessor :message
+
+        def initialize(type)
+          @message = "invalid type #{type}. type should be text, currency, date_time, image, document or video"
+          super
+        end
+      end
+
+      class MissingValue < StandardError
+        attr_reader :field, :message
+
+        def initialize(field, message)
+          @field = field
+          @message = message
+          super(message)
+        end
+      end
+
+      # Returns the parameter type.
+      #
+      # @returns type [String] Valid options are text, currency, date_time, image, document, video.
+      attr_accessor :type
+
+      module Type
+        TEXT = "text"
+        CURRENCY = "currency"
+        DATE_TIME = "date_time"
+        IMAGE = "image"
+        DOCUMENT = "document"
+        VIDEO = "video"
+
+        VALID_TYPES = [TEXT, CURRENCY, DATE_TIME, IMAGE, DOCUMENT, VIDEO].freeze
+      end
+
+      # Returns Text string if the parameter object type is text.
+      # For the header component, the character limit is 60 characters.
+      # For the body component, the character limit is 1024 characters.
+      #
+      # @returns text [String]
+      attr_accessor :text
+
+      # Returns Currency if the parameter object type is currency.
+      #
+      # @returns currency [Currency]
+      attr_accessor :currency
+
+      # Returns date_time if the parameter object type is date_time.
+      #
+      # @returns date_time [DateTime]
+      attr_accessor :date_time
+
+      # Returns image if the parameter object type is image.
+      #
+      # @returns image [Media]
+      attr_accessor :image
+
+      # Returns document if the parameter object type is document.
+      #
+      # @returns document [Media]
+      attr_accessor :document
+
+      # Returns video if the parameter object type is video.
+      #
+      # @returns video [Media]
+      attr_accessor :video
+
+      def initialize(type:, text: nil, currency: nil, date_time: nil, image: nil, document: nil, video: nil)
+        @type = type
+        @text = text
+        @currency = currency
+        @date_time = date_time
+        @image = image
+        @document = document
+        @video = video
+        validate
+      end
+
+      def to_json(*_args)
+        json = { type: type }
+        json[type.to_sym] = case type
+                            when "text"
+                              text
+                            when "currency"
+                              currency.to_json
+                            when "date_time"
+                              date_time.to_json
+                            when "image"
+                              image.to_json
+                            when "document"
+                              document.to_json
+                            when "video"
+                              video.to_json
+                            else
+                              raise "Invalid type: #{type}"
+                            end
+
+        json
+      end
+
+      private
+
+      def validate
+        validate_attributes
+        validate_type
+      end
+
+      def validate_type
+        return if Type::VALID_TYPES.include?(type)
+
+        raise InvalidType, type
+      end
+
+      def validate_attributes
+        [
+          [:text, text],
+          [:currency, currency],
+          [:date_time, date_time],
+          [:image, image],
+          [:document, document],
+          [:video, video]
+        ].each do |type_sym, value|
+          next unless type == type_sym
+          raise MissingValue.new(type, "#{type} is required when the type is #{type}") if value.nil?
+        end
+      end
+    end
+  end
+end
---
+++ test/whatsapp/resource/component_test.rb
@@ -0,0 +1,163 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require_relative '../../../lib/whatsapp_sdk/resource/parameter_object'
+require_relative '../../../lib/whatsapp_sdk/resource/media'
+require_relative '../../../lib/whatsapp_sdk/resource/date_time'
+require_relative '../../../lib/whatsapp_sdk/resource/currency'
+require_relative '../../../lib/whatsapp_sdk/resource/component'
+
+module WhatsappSdk
+  module Resource
+    module Resource
+      class ComponentTest < Minitest::Test
+        def test_button_index_is_set_to_0_by_default
+          button_component = WhatsappSdk::Resource::Component.new(
+            type: WhatsappSdk::Resource::Component::Type::BUTTON,
+            sub_type: WhatsappSdk::Resource::Component::Subtype::QUICK_REPLY
+          )
+
+          assert_equal(0, button_component.index)
+        end
+
+        def test_validation
+          error = assert_raises(WhatsappSdk::Resource::Component::InvalidField) do
+            WhatsappSdk::Resource::Component.new(
+              type: WhatsappSdk::Resource::Component::Type::HEADER,
+              sub_type: WhatsappSdk::Resource::Component::Subtype::QUICK_REPLY
+            )
+          end
+          assert_equal("sub_type is not required when type is not button", error.message)
+          assert_equal(:sub_type, error.field)
+
+          error = assert_raises(WhatsappSdk::Resource::Component::InvalidField) do
+            WhatsappSdk::Resource::Component.new(
+              type: WhatsappSdk::Resource::Component::Type::HEADER, index: 0
+            )
+          end
+          assert_equal("index is not required when type is not button", error.message)
+          assert_equal(:index, error.field)
+        end
+
+        def test_add_parameters
+          image = WhatsappSdk::Resource::Media.new(type: "image", link: "http(s)://URL", caption: "caption")
+          document = WhatsappSdk::Resource::Media.new(type: "document", link: "http(s)://URL", filename: "txt.rb")
+          video = WhatsappSdk::Resource::Media.new(type: "video", id: 123)
+          currency = WhatsappSdk::Resource::Currency.new(code: "USD", amount: 1000, fallback_value: "1000")
+          date_time = WhatsappSdk::Resource::DateTime.new(fallback_value: "2020-01-01T00:00:00Z")
+
+          parameter_text = WhatsappSdk::Resource::ParameterObject.new(type: "text", text: "I am a text")
+          parameter_currency = WhatsappSdk::Resource::ParameterObject.new(type: "currency", currency: currency)
+          parameter_date_time = WhatsappSdk::Resource::ParameterObject.new(type: "date_time", date_time: date_time)
+          parameter_image = WhatsappSdk::Resource::ParameterObject.new(type: "image", image: image)
+          parameter_document = WhatsappSdk::Resource::ParameterObject.new(type: "document", document: document)
+          parameter_video = WhatsappSdk::Resource::ParameterObject.new(type: "video", video: video)
+
+          header_component = WhatsappSdk::Resource::Component.new(type: WhatsappSdk::Resource::Component::Type::HEADER)
+
+          header_component.add_parameter(parameter_text)
+          header_component.add_parameter(parameter_currency)
+          header_component.add_parameter(date_time)
+          header_component.add_parameter(parameter_image)
+          header_component.add_parameter(parameter_document)
+          header_component.add_parameter(parameter_video)
+          header_component.add_parameter(parameter_date_time)
+
+          assert_equal(
+            [
+              parameter_text, parameter_currency, date_time, parameter_image,
+              parameter_document, parameter_video, parameter_date_time
+            ],
+            header_component.parameters
+          )
+        end
+
+        def test_to_json_header_component
+          image = WhatsappSdk::Resource::Media.new(type: "image", link: "http(s)://URL", caption: "caption")
+          parameter_image = WhatsappSdk::Resource::ParameterObject.new(type: "image", image: image)
+
+          header_component = WhatsappSdk::Resource::Component.new(
+            type: WhatsappSdk::Resource::Component::Type::HEADER,
+            parameters: [parameter_image]
+          )
+
+          assert_equal(
+            {
+              type: "header",
+              parameters: [
+                {
+                  type: "image",
+                  image: {
+                    link: "http(s)://URL",
+                    caption: "caption"
+                  }
+                }
+              ]
+            },
+            header_component.to_json
+          )
+        end
+
+        def test_to_json_body_component
+          parameter_text = WhatsappSdk::Resource::ParameterObject.new(type: "text", text: "I am a text")
+
+          currency = WhatsappSdk::Resource::Currency.new(code: "USD", amount: 1000, fallback_value: "1000")
+          parameter_currency = WhatsappSdk::Resource::ParameterObject.new(type: "currency", currency: currency)
+
+          date_time = WhatsappSdk::Resource::DateTime.new(fallback_value: "2020-01-01T00:00:00Z")
+          parameter_date_time = WhatsappSdk::Resource::ParameterObject.new(type: "date_time", date_time: date_time)
+
+          body_component = WhatsappSdk::Resource::Component.new(
+            type: WhatsappSdk::Resource::Component::Type::BODY,
+            parameters: [parameter_text, parameter_currency, parameter_date_time]
+          )
+
+          assert_equal(
+            {
+              type: "body",
+              parameters: [
+                {
+                  type: "text",
+                  text: "I am a text"
+                },
+                {
+                  type: "currency",
+                  currency: {
+                    fallback_value: "1000",
+                    code: "USD",
+                    amount_1000: 1000
+                  }
+                },
+                {
+                  type: "date_time",
+                  date_time: {
+                    fallback_value: "2020-01-01T00:00:00Z"
+                  }
+                }
+              ]
+            },
+            body_component.to_json
+          )
+        end
+
+        def test_to_json_button_component
+          button_component = WhatsappSdk::Resource::Component.new(
+            type: WhatsappSdk::Resource::Component::Type::BUTTON,
+            index: 0,
+            sub_type: WhatsappSdk::Resource::Component::Subtype::QUICK_REPLY,
+            parameters: [
+              WhatsappSdk::Resource::ButtonParameter.new(type: "payload", payload: "payload"),
+              WhatsappSdk::Resource::ButtonParameter.new(type: "text", text: "text")
+            ]
+          )
+
+          assert_equal(
+            { type: "button", parameters: [{ type: "payload", payload: "payload" }, { type: "text", text: "text" }],
+              sub_type: "quick_reply", index: 0 },
+            button_component.to_json
+          )
+        end
+      end
+    end
+  end
+end
