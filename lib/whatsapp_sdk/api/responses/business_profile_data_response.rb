c8f2939b88aa26736d21c5b1d80525dd5ac65d7d
Sahil <shlbnsl843@gmail.com>
2022-10-16 22:01:18 +0530

    feat: initial commit

---
+++ lib/whatsapp_sdk/api/responses/business_profile_data_response.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+# typed: strict
+
+require_relative "data_response"
+
+module WhatsappSdk
+  module Api
+    module Responses
+      class BusinessProfileDataResponse < DataResponse
+        sig { returns(String) }
+        attr_accessor :about
+
+        sig { returns(String) }
+        attr_accessor :address
+
+        sig { returns(String) }
+        attr_accessor :description
+
+        sig { returns(String) }
+        attr_accessor :email
+
+        sig { returns(String) }
+        attr_accessor :messaging_product
+
+        sig { returns(String) }
+        attr_accessor :profile_picture_url
+
+        sig { params(response: T::Hash[T.untyped, T.untyped]).void }
+        def initialize(response)
+          @about = T.let(response["data"][0]["about"], String)
+          @address = T.let(response["data"][0]["address"], String)
+          @description = T.let(response["data"][0]["description"], String)
+          @email = T.let(response["data"][0]["email"], String)
+          @messaging_product = T.let(response["data"][0]["messaging_product"], String)
+          @profile_picture_url = T.let(response["data"][0]["profile_picture_url"], String)
+          super(response)
+        end
+
+        sig { override.params(response: T::Hash[T.untyped, T.untyped]).returns(T.nilable(BusinessProfileDataResponse)) }
+        def self.build_from_response(response:)
+          return unless response["id"]
+
+          new(response)
+        end
+      end
+    end
+  end
+end
