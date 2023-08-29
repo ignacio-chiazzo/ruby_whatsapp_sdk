61e2ab842fd6dd5704d60e184d04a2959fe7a73e
emersonu <emerson.arj@gmail.com>
2023-08-29 20:40:46 -0300

    Returning Template Resource in TemplateDataResponse

--- lib/whatsapp_sdk/api/responses/template_data_response.rb
+++ lib/whatsapp_sdk/api/responses/template_data_response.rb
@@ -8,21 +8,15 @@ module WhatsappSdk
   module Api
     module Responses
       class TemplateDataResponse < DataResponse
-        sig { returns(String) }
-        attr_accessor :id
-
-        sig { returns(T.nilable(WhatsappSdk::Resource::Template::Status)) }
-        attr_accessor :status
-
-        sig { returns(T.nilable(WhatsappSdk::Resource::Template::Category)) }
-        attr_accessor :category
+        sig { returns(::WhatsappSdk::Resource::Template) }
+        attr_reader :template
 
         sig { params(response: T::Hash[T.untyped, T.untyped]).void }
-        def initialize(response)
+        def initialize(response:)
           @id = T.let(response["id"], String)
           @status = WhatsappSdk::Resource::Template::Status.try_deserialize(response["status"])
           @category = WhatsappSdk::Resource::Template::Category.try_deserialize(response["category"])
-          
+
           super(response)
         end
 
@@ -31,6 +25,15 @@ module WhatsappSdk
           return unless response["id"]
 
           new(response)
+        end
+
+        private
+
+        sig { params(template_json: T::Hash[T.untyped, T.untyped]).returns(::WhatsappSdk::Resource::Template) }
+        def parse_template(template_json)
+          ::WhatsappSdk::Resource::Template.new(id: template_json["id"],
+                                                status: template_json["status"],
+                                                category: template_json["category"])
         end
       end
     end
